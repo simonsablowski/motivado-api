7b212795e4fc61fb21bfebe04a4be470f4c0f398
simsab@simsab.de <simsab@simsab.de@f3ced28d-84fd-1554-0473-eeb05d68dc4e>
2011-01-21 23:13:52 +0000

    kicked out unneeded logic

--- Helpers/Error.php
+++ Helpers/Error.php
@@ -30,21 +30,11 @@ class Error extends Exception {
 		return $this->type = $value;
 	}
 	
-	/*protected function convertArrayToString($array) {
-		ob_start();
-		print_r($array);
-		$output = ob_get_contents();
-		ob_end_clean();
-		return $output;
-	}*/
-	
 	public function __toString() {
 		$string = '';
 		$string .= sprintf("\n\t<errortype>%s</errortype>", $this->getType());
 		$string .= sprintf("\n\t<errorcode>%s</errorcode>", $this->getCode());
 		$string .= sprintf("\n\t<errormessage>%s</errormessage>", $this->getMessage());
-		/*$string .= sprintf("\n%s", !is_array($this->getDetails()) ? $this->getDetails() : $this->convertArrayToString($this->getDetails()));
-		$string .= sprintf("\n%s", $this->getTraceAsString());*/
 		return $string;
 	}
 	
