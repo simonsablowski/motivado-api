2b60046124b7838588d83cb27dbfcb0e15300e78
Simon Sablowski <contact@simsab.net>
2011-01-22 19:09:50 +0000

    simplified class using magic __callStatic method

--- Libraries/Database.php
+++ Libraries/Database.php
@@ -6,6 +6,10 @@ class Database {
 	protected static $link = NULL;
 	protected static $requiredFields = array();
 	
+	public static function __callStatic($method, $parameters) {
+		return call_user_func_array(array(self::getClassName(), $method), $parameters);
+	}
+	
 	final public static function initialize($configuration) {
 		self::setClassName($configuration['type'] . 'Database');
 		
@@ -36,45 +40,5 @@ class Database {
 	
 	final protected static function setLink($link) {
 		return self::$link = $link;
-	}
-	
-	protected static function getRequiredFields() {
-		return call_user_func_array(array(self::getClassName(), 'getRequiredFields'), array());
-	}
-	
-	public static function connect() {
-		return call_user_func_array(array(self::getClassName(), 'connect'), array());
-	}
-	
-	public static function query($statement) {
-		return call_user_func_array(array(self::getClassName(), 'query'), array($statement));
-	}
-	
-	public static function fetch($result) {
-		return call_user_func_array(array(self::getClassName(), 'fetch'), array($result));
-	}
-	
-	public static function count($result) {
-		return call_user_func_array(array(self::getClassName(), 'count'), array($result));
-	}
-	
-	public static function escape($value) {
-		return call_user_func_array(array(self::getClassName(), 'escape'), array($value));
-	}
-	
-	public static function select() {
-		return call_user_func_array(array(self::getClassName(), 'select'), func_get_args());
-	}
-	
-	public static function insert() {
-		return call_user_func_array(array(self::getClassName(), 'insert'), func_get_args());
-	}
-	
-	public static function update() {
-		return call_user_func_array(array(self::getClassName(), 'update'), func_get_args());
-	}
-	
-	public static function delete() {
-		return call_user_func_array(array(self::getClassName(), 'delete'), func_get_args());
 	}
 }
