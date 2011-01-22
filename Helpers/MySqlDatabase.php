72c0213e9ed334ccf7b975838a4e3c3346d27de5
simsab@simsab.de <simsab@simsab.de@f3ced28d-84fd-1554-0473-eeb05d68dc4e>
2011-01-22 02:31:38 +0000

    handling Database connection Errors

--- Helpers/MySqlDatabase.php
+++ Helpers/MySqlDatabase.php
@@ -1,9 +1,22 @@
 <?php
 
 class MySqlDatabase extends SqlDatabase {
+	protected static function connectToHost($host, $user, $password) {
+		return mysql_connect($host, $user, $password);
+	}
+	
+	protected static function selectDatabase($name, $link) {
+		return mysql_select_db($name, $link);
+	}
+	
 	public static function connect() {
-		self::setLink(mysql_connect(self::getConfiguration('host'), self::getConfiguration('user'), self::getConfiguration('password')));
-		mysql_select_db(self::getConfiguration('name'), self::getLink());
+		if (!self::setLink(self::connectToHost(self::getConfiguration('host'), self::getConfiguration('user'), self::getConfiguration('password')))) {
+			throw new Error('Cannot connect to database host', self::getConfiguration('host'));
+		}
+		
+		if (!self::selectDatabase(self::getConfiguration('name'), self::getLink())) {
+			throw new Error('Cannot select database', self::getConfiguration('name'));
+		}
 	}
 	
 	public static function query($statement) {
