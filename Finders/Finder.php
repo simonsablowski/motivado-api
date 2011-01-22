badf3b1cecc6b1b32cec879266d08f561030ab41
simsab@simsab.de <simsab@simsab.de@f3ced28d-84fd-1554-0473-eeb05d68dc4e>
2011-01-22 16:23:54 +0000

    shortened methods

--- Finders/Finder.php
+++ Finders/Finder.php
@@ -37,13 +37,14 @@ abstract class Finder extends Application {
 		return strtolower(self::getModelName());
 	}
 	
+	protected static function resolveCondition($condition) {
+		if (is_null($condition)) return self::getDefaultCondition();
+		else return array_merge(self::getDefaultCondition(), $condition);
+	}
+	
 	public static function findAll($condition = NULL, $sorting = NULL, $limit = NULL) {
-		if (is_null($condition)) $condition = self::getDefaultCondition();
-		else $condition = array_merge(self::getDefaultCondition(), $condition);
-		if (is_null($sorting)) $sorting = self::getDefaultSorting();
-		
 		$Objects = array();
-		$result = Database::select(self::getTableName(), $condition, $limit, $sorting);
+		$result = Database::select(self::getTableName(), self::resolveCondition($condition), $limit, !is_null($sorting) ? $sorting : self::getDefaultSorting());
 		while ($row = Database::fetch($result)) {
 			$modelName = self::getModelName();
 			$Objects[] = new $modelName($row);
@@ -52,25 +53,18 @@ abstract class Finder extends Application {
 	}
 	
 	public static function countAll($condition = NULL) {
-		if (is_null($condition)) $condition = self::getDefaultCondition();
-		else $condition = array_merge(self::getDefaultCondition(), $condition);
-		
-		$result = Database::select(self::getTableName(), $condition);
+		$result = Database::select(self::getTableName(), self::resolveCondition($condition));
 		return Database::count($result);
 	}
 	
 	public static function findFirst($condition = NULL, $sorting = NULL) {
 		$Objects = self::findAll($condition, $sorting, 1);
-		if ($Objects) return $Objects[0];
+		if ($Objects) return pos($Objects);
 		else throw new Error('Record not found', $condition);
 	}
 	
 	public static function find($primaryKeyValue, $condition = NULL) {
-		if (is_null($condition)) $condition = self::getDefaultCondition();
-		else $condition = array_merge(self::getDefaultCondition(), $condition);
-		if (!is_array($primaryKeyValue)) $primaryKeyValue = array(self::getPrimaryKey() => $primaryKeyValue);
-		$condition = array_merge($primaryKeyValue, $condition);
-		
+		$condition = array_merge(is_array($primaryKeyValue) ? $primaryKeyValue : array(self::getPrimaryKey() => $primaryKeyValue), self::resolveCondition($condition));
 		$result = Database::select(self::getTableName(), $condition, 1);
 		$row = Database::fetch($result);
 		if (!$row) throw new Error('Record not found', array('$primaryKeyValue' => $primaryKeyValue, '$condition' => $condition));
@@ -80,11 +74,8 @@ abstract class Finder extends Application {
 	}
 	
 	public static function findBy($fields, $values, $condition = NULL) {
-		if (is_null($condition)) $condition = self::getDefaultCondition();
-		else $condition = array_merge(self::getDefaultCondition(), $condition);
-		$condition = array_merge(array_combine($fields, $values), $condition);
-		
-		$result = Database::select(self::getTableName(), $condition, 1);echo mysql_error();
+		$condition = array_merge(array_combine($fields, $values), self::resolveCondition($condition));
+		$result = Database::select(self::getTableName(), $condition, 1);
 		$row = Database::fetch($result);
 		if (!$row) throw new Error('Record not found', array('$fields' => $fields, '$values' => $values, '$condition' => $condition));
 		
