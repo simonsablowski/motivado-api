6fa843532b34c871b0089b2f53eb8ae647f69134
Simon Sablowski <contact@simsab.net>
2011-01-23 13:54:50 +0000

    - added field condition for ObjectSequence - minor changes

--- configuration.php
+++ configuration.php
@@ -1,18 +1,16 @@
 <?php
 
-$configuration = array();
-
-$configuration['header'] = 'Content-Type: text/xml; charset=utf-8';
-
-$configuration['Database'] = array(
-	'type' => 'MySql',
-	'host' => 'localhost',
-	'name' => 'motivado_api',
-	'user' => 'root',
-	'password' => ''
-);
-
-$configuration['Request'] = array(
-	'defaultController' => 'Coaching',
-	'defaultAction' => 'index'
+$configuration = array(
+	'header' => 'Content-Type: text/xml; charset=utf-8',
+	'Database' => array(
+		'type' => 'MySql',
+		'host' => 'localhost',
+		'name' => 'motivado_api',
+		'user' => 'root',
+		'password' => ''
+	),
+	'Request' => array(
+		'defaultController' => 'Coaching',
+		'defaultAction' => 'index'
+	)
 );
