9b5cd41da4d40a4f6b611a8ba67a4e7b394bfbdd
Simon Sablowski <contact@simsab.net>
2011-01-23 18:37:36 +0000

    - made header configuration optional - added validation for Controller method

--- Controllers/Controller.php
+++ Controllers/Controller.php
@@ -25,9 +25,12 @@ abstract class Controller extends Application {
 	}
 	
 	protected function performAction($actionName, $parameters) {
-		$ReflectionMethod = new ReflectionMethod($this, $actionName);
+		$Reflection = new ReflectionClass($this);
+		if (!$Reflection->hasMethod($actionName)) {
+			throw new FatalError('Invalid action', array('Controller' => $this->getClassName(), 'Action' => $actionName, 'Parameters' => $parameters));
+		}
 		
-		if (count($ReflectionMethod->getParameters()) <= count($parameters)) {
+		if (count($Reflection->getMethod($actionName)->getParameters()) <= count($parameters)) {
 			call_user_func_array(array($this, $actionName), $parameters);
 		} else {
 			throw new FatalError('Missing parameters', array('Controller' => $this->getClassName(), 'Action' => $actionName, 'Parameters' => $parameters));
