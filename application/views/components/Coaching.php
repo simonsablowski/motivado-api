9150fbde77b635a617ae0397e816b61e84ba4d61
simsab@simsab.de <simsab@simsab.de@f3ced28d-84fd-1554-0473-eeb05d68dc4e>
2011-02-19 13:53:12 +0000

    components no longer needed

--- application/views/components/Coaching.php
+++
@@ -1,5 +0,0 @@
-	<?/*<coaching id="<? echo $Coaching->getId(); ?>">
-	<? foreach ($Coaching->getData() as $field => $value): ?>
-		<<? echo strtolower($field); ?>><? echo Xml::cleanProperty($value); ?></<? echo strtolower($field); ?>>
-	<? endforeach; ?>
-	</coaching>*/?>
