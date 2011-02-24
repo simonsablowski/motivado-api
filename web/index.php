de1e241878efee9ee872cad1d02c14748c5590e3
Simon Sablowski <contact@simsab.net>
2011-02-24 00:18:51 +0000

    improved Application including

--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);
 
 require_once dirname(__FILE__) . '/../configuration.php';
 foreach ($configuration['includeDirectories'] as $includeDirectory) {
-	if (file_exists($filePath = $includeDirectory . 'Application.php')) include $filePath;
+	if (file_exists($filePath = $includeDirectory . 'Application.php')) break include $filePath;
 }
 
 $Application = new Application($configuration, isset($_GET['localization']) ? $_GET['localization'] : NULL);
