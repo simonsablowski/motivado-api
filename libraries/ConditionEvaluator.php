e9d7db2e3a390b33ce88dccbb87236be3787484d
simsab@simsab.de <simsab@simsab.de@f3ced28d-84fd-1554-0473-eeb05d68dc4e>
2011-02-25 20:35:04 +0000

    improved ConditionEvaluator

--- libraries/ConditionEvaluator.php
+++ libraries/ConditionEvaluator.php
@@ -19,37 +19,43 @@ class ConditionEvaluator extends Application {
 	
 	protected function secureCondition(&$condition) {
 		if (preg_match('/[^\s\w()\']/i', $condition)) {
-			throw new FatalError('Condition contains invalid characters', $condition);
+			throw new Error('Condition contains invalid characters', $condition);
 		}
 		
 		foreach ($this->getOperators() as $alias => $original) {
 			$condition = preg_replace(sprintf('/(\s+)%s(\s+)/', $alias), sprintf('$1%s$2', $original), $condition);
 		}
 		
-		if (!preg_match_all('/(^|\s|\()([a-z]{1}\w+)/i', $condition, $variables)) {
-			throw new FatalError('Condition contains no valid variables', $condition);
+		if (!preg_match_all('/(^|\s|\()([a-z]{1}\w+)([^\s\)$])?/i', $condition, $variables)) {
+			throw new Error('Condition contains no valid variables', $condition);
+		} else {
+			foreach ($variables[3] as $n => $element) {
+				if ($element) {
+					throw new Error('Condition contains invalid element', $variables[0][$n]);
+				}
+			}
 		}
 		
 		foreach ($variables[0] as $n => $part) {
-			if (preg_match(sprintf('/%s([^\s\)$]+)/', $part), $condition, $element)) {
-				throw new FatalError('Condition contains invalid element', $element);
-			}
-			
 			$variable = $variables[2][$n];
 			$condition = str_replace($part, str_replace($variable, '$' . $variable, $part), $condition);
 		}
 		
-		$condition = sprintf('return %s;', $condition);
+		return $condition = sprintf('return %s;', $condition);
 	}
 	
 	public function evaluate($condition) {
-		extract($this->getCoachingConfigurator()->getValues());
-		
-		$level = error_reporting(0);
-		$this->secureCondition($condition);
-		$result = (bool)eval($condition);
-		error_reporting($level);
-		
-		return $result;
+		try {
+			extract($this->getCoachingConfigurator()->getValues());
+			
+			$level = error_reporting(0);
+			$this->secureCondition($condition);
+			$result = (bool)eval($condition);
+			error_reporting($level);
+			
+			return $result;
+		} catch (Error $Error) {
+			return FALSE;
+		}
 	}
 }
