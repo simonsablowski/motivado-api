06b873322e45a9b3352130807d56a73b395c1064
Simon Sablowski <contact@simsab.net>
2011-03-10 21:33:10 +0000

    added getter and setter methods for CoachingConfigurator

--- libraries/CoachingConfigurator.php
+++ libraries/CoachingConfigurator.php
@@ -1,13 +1,29 @@
 <?php
 
 class CoachingConfigurator extends Application {
+	protected $values = array();
+	
 	public function __construct() {
 		
 	}
 	
 	public function getValues() {
-		return array(
-			'var' => 1
-		);
+		return $this->values;
+	}
+	
+	public function getValue($field = NULL) {
+		if (is_null($field)) return $this->getValues();
+		
+		return isset($this->values[$field]) ? $this->values[$field] : NULL;
+	}
+	
+	public function setValues($values) {
+		return $this->values = array_merge($this->getValues(), $values);
+	}
+	
+	public function setValue($field, $value = NULL) {
+		if (is_null($value)) return $this->setValues($field);
+		
+		return $this->values[$field] = $value;
 	}
 }
