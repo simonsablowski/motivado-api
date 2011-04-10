58022b77f966d009e6b6483e05f5e262771dd958
simsab@simsab.de <simsab@simsab.de@f3ced28d-84fd-1554-0473-eeb05d68dc4e>
2011-04-10 00:59:37 +0000

    translating Error message and type

--- views/Error.show.php
+++ views/Error.show.php
@@ -2,8 +2,8 @@
 	'response' => array(
 		'status' => $this->localize('Failure'),
 		'error' => array(
-			'type' => $Error->getType(),
-			'message' => $Error->getMessage()
+			'type' => $this->localize($Error->getType()),
+			'message' => $this->localize($Error->getMessage())
 		)
 	)
 )); ?>
