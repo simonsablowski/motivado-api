aeecaf791da48f2a688f36bb51ab1e9a42241d12
Simon Sablowski <contact@simsab.net>
2011-05-03 15:51:08 +0000

    reduced response for Error

--- views/Error.show.php
+++ views/Error.show.php
@@ -1,9 +1,6 @@
 <? echo Json::encode(array(
-	'response' => array(
-		'status' => $this->localize('Failure'),
-		'error' => array(
-			'type' => $this->localize($Error->getType()),
-			'message' => $this->localize($Error->getMessage())
-		)
+	'error' => array(
+		'type' => $this->localize($Error->getType()),
+		'message' => $this->localize($Error->getMessage())
 	)
-)); ?>
+));
